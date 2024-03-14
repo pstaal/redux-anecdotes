b1d23135f86756da2924d70d239d75fa1a2b593a
peter staal <peterstaal@MBP-van-peter.home>
2024-03-14 17:05:20 +0100

    exercise 6.3-6.8

---
+++ src/components/AnecdoteList.jsx
@@ -0,0 +1,25 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addVote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const orderedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
+
+  return (
+    <>
+      {orderedAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(addVote(anecdote.id))}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default AnecdoteList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import reducer from "./reducers/anecdoteReducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
