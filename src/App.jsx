8c508673ed38dcbb42d3f9add149f4f32305a943
peter staal <peterstaal@MBP-van-peter.home>
2024-03-15 09:44:46 +0100

    exercise 6.9

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useSelector } from "react-redux";
-import { addVote } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecDoteForm";
 import AnecdoteList from "./components/AnecdoteList";
+import Filter from "./components/Filter";
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
